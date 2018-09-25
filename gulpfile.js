5bdc8108bb373d7057ce921de69b01c427c5c764
sradms0 <sradms0@openmailbox.org>
2018-09-25 19:14:37 -0400

    refactor concatScripts and minifyScripts into scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,15 @@ const gulp = require('gulp'),
     maps = require('gulp-sourcemaps'),
     del = require('del');
 
-gulp.task('concatScripts', () => {
+gulp.task('scripts', () => {
     return gulp.src(['js/jquery.js', 'js/circle/**'])
         .pipe(maps.init())
         .pipe(concat('all.js'))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('minifyScripts', ['concatScripts'], () => {
-    return gulp.src(['js/all.js'])
         .pipe(uglify())
         .pipe(rename('all.min.js'))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('scripts', ['minifyScripts'], () => {
-    return gulp.src(['js/all.min.js'], {base: './js'})
+        .pipe(maps.write('./'))
         .pipe(gulp.dest('dist/scripts'));
 });
-
-gulp.task('concatStyles', () => {
-    return gulp.src(['sass/global.scss'])
-        .pipe(maps.init())
-        .pipe(concat('all.css'))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest('css'));
-});
-
 
 gulp.task('styles', () => {
     return gulp.src('sass/global.scss')
@@ -47,5 +28,5 @@ gulp.task('styles', () => {
 })
 
 gulp.task('clean', () => {
-    del(['dist', 'js/all*']);
+    del(['dist']);
 })
