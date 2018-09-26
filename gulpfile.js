5691fc46ddbfedb3b0095b4400ea29e407419472
sradms0 <sradms0@openmailbox.org>
2018-09-26 14:54:54 -0400

    refactor for live loading

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,53 @@ const gulp = require('gulp'),
     connect = require('gulp-connect'),
     del = require('del');
 
+const sassSources = ['sass/**/*.scss'],
+        jsSources = ['js/jquery.js', 'js/circle/**'],
+        imgSources = ['images/*'],
+        htmlSources = ['*.html'];
+
+gulp.task('html', () => {
+    return gulp.src(htmlSources)
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
+});
+
 gulp.task('scripts', () => {
-    return gulp.src(['js/jquery.js', 'js/circle/**'])
+    return gulp.src(jsSources)
         .pipe(maps.init())
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(rename('all.min.js'))
         .pipe(maps.write('./'))
-        .pipe(gulp.dest('dist/scripts'));
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(connect.reload());
 });
 
 gulp.task('styles', () => {
-    return gulp.src('sass/global.scss')
+    return gulp.src(sassSources)
         .pipe(maps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(concat('all.min.css'))
         .pipe(maps.write('./'))
-        .pipe(gulp.dest('dist/styles'));
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(connect.reload());
 });
 
 gulp.task('images', () => {
-    return gulp.src('images/*')
+    return gulp.src(imgSources)
         .pipe(imagemin())
         .pipe(gulp.dest('dist/content'));
 });
 
-gulp.task('watchSass', () => {
-    gulp.watch('sass/**/*.scss', ['styles']);
+gulp.task('watch', () => {
+    gulp.watch(jsSources, ['scripts']);
+    gulp.watch(sassSources, ['styles']);
 });
 
-gulp.task('build', ['clean', 'scripts', 'styles', 'images'], () => {
-    return gulp.src('index.html', {base: './'})
-        .pipe(gulp.dest('dist'));
-})
+gulp.task('build', ['clean', 'html', 'scripts', 'styles', 'images']);
 
 gulp.task('clean', () => {
-    del(['dist']);
+    return del(['dist']);
 })
 
 gulp.task('connect', ['build'], () => {
@@ -56,4 +67,4 @@ gulp.task('connect', ['build'], () => {
     });
 });
 
-gulp.task('default', ['watchSass', 'connect'], () => {});
+gulp.task('default', ['connect']);
